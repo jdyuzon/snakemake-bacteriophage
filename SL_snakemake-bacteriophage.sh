89c8bebe34d8250c4fbfb414684891dcaa8a6036
jdyuzon <53333153+jdyuzon@users.noreply.github.com>
2023-06-05 10:07:10 -0700

    Update SL_snakemake-bacteriophage.sh

--- SL_snakemake-bacteriophage.sh
+++ SL_snakemake-bacteriophage.sh
@@ -5,12 +5,15 @@
 #SBATCH --constraint cpu
 #SBATCH --ntasks-per-node=128
 #SBATCH --cpus-per-task=2
-#SBATCH -t 00:10:00
+#SBATCH -t 00:30:00
 #SBATCH -J snakemake-bacteriophage
 
+
+##conda create -c conda-forge -c bioconda -n snakemake-bacteriophage snakemake
+##conda install -n snakemake-bacteriophage -c conda-forge -c bioconda genomad
 
 source /global/homes/j/jdyuzon/.bashrc
 conda activate snakemake-bacteriophage
 
 snakemake --unlock
-snakemake --cores 128 --rerun-incomplete --forceall
+snakemake --cores 32 --rerun-incomplete --forceall -k
